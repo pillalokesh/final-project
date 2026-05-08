c5764dadcca30b01961e44595207d137d8b002a2
pillalokesh <pillalokesh3@gmail.com>
2026-05-08 13:16:45 +0530

    2

--- alb.tf
+++ alb.tf
@@ -74,12 +74,14 @@ resource "aws_lb_listener" "https" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = aws_acm_certificate.main.arn
+  certificate_arn   = aws_acm_certificate_validation.main.certificate_arn
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.frontend.arn
   }
+
+  depends_on = [aws_acm_certificate_validation.main]
 }
 
 resource "aws_lb_listener_rule" "backend" {
